8111b723dca588bbd806037b7b23da105487a79b
Michelle Wong <michellekwong2@gmail.com>
2020-08-03 13:39:37 -0400

    Fixed fetch request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import WeatherFetch from './weatherFetch.js'
 
+const dotenv = require('dotenv').config()
+
 function App() {
   return (
     <div className="App">
