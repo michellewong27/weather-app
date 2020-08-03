509bea2df452a5dace4b754071f01fb5c8ba4728
Michelle Wong <michellekwong2@gmail.com>
2020-08-03 12:15:25 -0400

    creating hooks

--- src/weatherFetch.js
+++ src/weatherFetch.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 
-function useEffect() {
-    fetch('api.openweathermap.org/data/2.5/weather?q={Karachi}&appid=APP_KEY')
-
+function WeatherFetch() {
+    const key = 'REACT_APP_API_KEY';
+    const [feels_like,setFeelsLike] = useState('');
+    const [mainTemp,setMainTemp] = useState('');
+    const [description,setDescription] = useState('');
+    const [main,setMain] = useState('');
+    const [iconID,setIconID] = useState('');
+    useEffect(() => {
+        fetch('api.openweathermap.org/data/2.5/weather?q={Karachi}&appid=APP_KEY')
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+        })
+    },[])
+    return (
+        <>
+        </>
+    )
 }
 
-export default useEffect
+export default WeatherFetch;
